b6c0d4e57b555ce295d6f31a0ad3c1df389868d6
Daniel Jorge <djpgonzaga@gmail.com>
2019-04-18 01:28:27 -0300

    add new dumbs components for project

--- src/app/dashboard-users/dashboard-users.module.ts
+++ src/app/dashboard-users/dashboard-users.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// Containers
 import { UserDashboardComponent } from './containers/user-dashboard/user-dashboard.component';
+
+// Components
+import { PassengerCountComponent} from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent} from "./components/passenger-detail/passenger-detail.component";
 
 @NgModule({
   declarations: [
-    UserDashboardComponent
+    UserDashboardComponent,
+    PassengerDetailComponent,
+    PassengerCountComponent
   ],
   exports: [
-    UserDashboardComponent
+    UserDashboardComponent,
   ],
   imports: [
     CommonModule,
