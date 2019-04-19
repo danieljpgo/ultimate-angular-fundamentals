f86cacc6881f70024794b1e01827a341ab52904e
danieljpgo <djpgonzaga@hotmail.com>
2019-04-19 19:51:58 -0300

    data services and dependency injection

--- src/app/dashboard-users/containers/user-dashboard/user-dashboard.component.ts
+++ src/app/dashboard-users/containers/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+import { DashboardUsersService } from '../../dashboard-users.service'
+
 import { Passenger } from '../../models/user.interface';
 
 @Component({
@@ -14,43 +17,11 @@ export class UserDashboardComponent implements OnInit {
   name = '';
   passengers: Passenger[] = [];
 
-  constructor() { }
+  constructor(private passengerService: DashboardUsersService) { }
 
   ngOnInit() {
     // console.log('ngOnInit')
-    this.passengers = [{
-      id: 1,
-      fullName: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1452292999,
-      children: [{ name: 'Ted', age: 12}]
-    }, {
-      id: 2,
-      fullName: 'Lucas',
-      checkedIn: true,
-      checkInDate: 1452292999,
-      children: [{ name: 'Luis', age: 12}, { name: 'Voce', age: 15}]
-    }, {
-      id: 3,
-      fullName: 'James',
-      checkedIn: false,
-    }]
-  }
-  handleInput(event: any) {
-    this.name = event.target.value;
-  }
-  handleClick() {
-    this.name = 'Catra';
-  }
-  handleClickRef(value: string) {
-    // console.log(value);
-  }
-  handleChange() {
-    this.name = 'sss';
-  }
-
-  handleChange2(value: string) {
-    this.name = value;
+    this.passengers = this.passengerService.getPassengers();
   }
 
   handleRemove(event: Passenger) {
@@ -65,4 +36,21 @@ export class UserDashboardComponent implements OnInit {
       return passenger;
     })
   }
+
+  // handleInput(event: any) {
+  //   this.name = event.target.value;
+  // }
+  // handleClick() {
+  //   this.name = 'Catra';
+  // }
+  // handleClickRef(value: string) {
+  //   // console.log(value);
+  // }
+  // handleChange() {
+  //   this.name = 'sss';
+  // }
+  //
+  // handleChange2(value: string) {
+  //   this.name = value;
+  // }
 }
--- src/app/dashboard-users/dashboard-users.module.ts
+++ src/app/dashboard-users/dashboard-users.module.ts
@@ -6,8 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { UserDashboardComponent } from './containers/user-dashboard/user-dashboard.component';
 
 // Components
-import { PassengerCountComponent} from "./components/passenger-count/passenger-count.component";
-import { PassengerDetailComponent} from "./components/passenger-detail/passenger-detail.component";
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+
+// Services
+import { DashboardUsersService } from './dashboard-users.service';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { PassengerDetailComponent} from "./components/passenger-detail/passenger
   imports: [
     CommonModule,
     FormsModule
+  ],
+  providers: [
+    DashboardUsersService
   ]
 })
 export class DashboardUsersModule { }
