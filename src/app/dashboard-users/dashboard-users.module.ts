e2a65e09a7e72abcb3a958fc2f5816981ad9cfa1
Daniel Jorge <djpgonzaga@gmail.com>
2019-04-19 19:57:29 -0300

    add services files

--- src/app/dashboard-users/dashboard-users.module.ts
+++ src/app/dashboard-users/dashboard-users.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 // Containers
 import { UserDashboardComponent } from './containers/user-dashboard/user-dashboard.component';
@@ -23,7 +23,7 @@ import { DashboardUsersService } from './dashboard-users.service';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    HttpClientModule
   ],
   providers: [
     DashboardUsersService
